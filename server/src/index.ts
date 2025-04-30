3a259bbf32c1eaf56c8ccb10d8bc871b78e566fd
Kent Garcia <kentdgarcia@iskolarngbayan.pup.edu.ph>
2025-04-30 22:39:13 +0800

    Initialize server with Apollo GraphQL setup and TypeScript configuration
    - Created package.json with dependencies for Apollo Server and TypeScript.
    - Added TypeScript configuration file (tsconfig.json) for project compilation.
    - Implemented basic GraphQL schema and sample data in index.ts for querying books.

---
+++ server/src/index.ts
@@ -0,0 +1,6 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
